d9b6640bdb4e45cf33e9530fcdceb23915b2ccb6
raoufboussa <fa_boussa@esi.dz>
2019-11-26 01:24:19 +0100

    add the cart fonctionality

--- core/models.py
+++ core/models.py
@@ -34,9 +34,26 @@ class Item(models.Model):
             "slug": self.slug
         })
 
+    def get_add_to_cart_url(self):
+        return reverse("core:add_to_cart", kwargs={
+            "slug": self.slug
+        })
+
+    def get_remove_from_cart_url(self):
+        return reverse("core:remove_from_cart", kwargs={
+            "slug": self.slug
+        })
+
 
 class OrderItem(models.Model):
-    Item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, blank=True, null=True)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.title}"
 
 
 class Order(models.Model):
